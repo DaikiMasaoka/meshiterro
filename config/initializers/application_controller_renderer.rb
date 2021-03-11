47df93b1785693d2c4a89ccfaabc234bbdc8b127
DaikiMasaoka <masadi.29@icloud.com>
2021-03-11 13:38:11 +0000

    [Strat]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '00944aa60ad2ac752b5c0840550c38814a06823f224999a43b1aae9b9592dc160f3079de8a942637a6ef5ed1793959c8171d846f3bdc0bb87c48e9ab1397ff3d'
